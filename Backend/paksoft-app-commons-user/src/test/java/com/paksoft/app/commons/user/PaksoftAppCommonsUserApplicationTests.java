bc60dcca3a82b095d4d02809e445f965a3fa52ce
AldoRoblesA <patrikrobles@gmail.com>
2020-07-29 20:35:24 -0500

    ok

---
+++ Backend/paksoft-app-commons-user/src/test/java/com/paksoft/app/commons/user/PaksoftAppCommonsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paksoft.app.commons.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaksoftAppCommonsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
