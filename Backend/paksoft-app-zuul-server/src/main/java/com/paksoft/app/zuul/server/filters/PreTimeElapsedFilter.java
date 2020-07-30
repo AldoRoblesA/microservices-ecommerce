c75f27cc31033f0f8a34126bebbbf234a5a838fd
AldoRoblesA <patrikrobles@gmail.com>
2020-07-29 20:42:00 -0500

    ok

---
+++ Backend/paksoft-app-zuul-server/src/main/java/com/paksoft/app/zuul/server/filters/PreTimeElapsedFilter.java
@@ -0,0 +1,5 @@
+package com.paksoft.app.zuul.server.filters;
+
+public class PreTimeElapsedFilter {
+
+}
---
+++ Backend/paksoft-app-zuul-server/src/test/java/com/paksoft/app/zuul/server/PaksoftAppServiceZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paksoft.app.zuul.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaksoftAppServiceZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
